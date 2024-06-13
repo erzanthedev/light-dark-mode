68c41611f2676db5ba4d367d32bbdd269179ff88
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-06-13 11:43:28 +1000

    added more logic to change imageMode

--- script.js
+++ script.js
@@ -7,12 +7,20 @@ const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
 const textBox = document.getElementById("text-box");
 
+// Dark or Light Images
+function imageMode(color) {
+  image1.src = `img/undraw_conceptual_idea_${color}.svg`;
+  image2.src = `img/undraw_feeling_proud_${color}.svg`;
+  image3.src = `img/undraw_proud_coder_${color}.svg`;
+}
+
 // Dark Mode Styles
 function darkMode() {
   nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
   textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
   toggleIcon.children[0].textContent = "Dark Mode";
   toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
+  imageMode("dark");
 }
 // Light Mode Styles
 function lightMode() {
@@ -20,11 +28,12 @@ function lightMode() {
   textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
   toggleIcon.children[0].textContent = "Light Mode";
   toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+  imageMode("light");
 }
 // Switch Theme dynamically
 function switchTheme(event) {
   if (event.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark"); // Changes theme from top level of document.
     darkMode();
   } else {
     document.documentElement.setAttribute("data-theme", "light");
